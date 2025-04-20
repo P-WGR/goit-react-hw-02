ed21790bb93539b0975df62d8e3ca357a27229bc
Piotr Wagner <wgr.animations@gmail.com>
2025-04-20 10:58:31 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,43 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import Description from "./components/Description";
+import React, { useState, useEffect } from "react";
 import Feedback from "./components/Feedback";
 import Options from "./components/Options";
 import Notification from "./components/Notification";
 
-const initialValue = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
-
 function App() {
-  const [rate, setRate] = useState(() => {
-    const savedRates = localStorage.getItem("Feedback rates");
-    if (savedRates !== null) {
-      return JSON.parse(savedRates);
-    } else {
-      return initialValue;
-    }
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    return savedFeedback || { good: 0, neutral: 0, bad: 0 };
   });
 
-  const updateFeedback = (x) => {
-    setRate({ ...rate, [x]: rate[x] + 1 });
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === "reset") {
+      setFeedback({ good: 0, neutral: 0, bad: 0 });
+      localStorage.removeItem("feedback");
+      return;
+    }
+
+    setFeedback((prevFeedback) => {
+      const updatedFeedback = {
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      };
+      localStorage.setItem("feedback", JSON.stringify(updatedFeedback));
+      return updatedFeedback;
+    });
   };
 
-  const totalFeedback = rate.good + rate.neutral + rate.bad;
-  const percentRate = Math.round(
-    ((rate.good + rate.neutral) / totalFeedback) * 100
-  );
-
-  const resetRate = () => {
-    setRate(initialValue);
-    localStorage.clear();
-  };
-
-  useEffect(() => {
-    localStorage.setItem("Feedback rates", JSON.stringify(rate));
-  }, [rate]);
-
-  // useEffect(() => {
-  //   const savedRates = localStorage.getItem("Feedback rates");
-  //   if (savedRates !== null) {
-  //     setRate(JSON.parse(savedRates));
-  //   }
-  // }, []);
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
   return (
-    <>
-      <Description />
-      <Options updateFeedback={updateFeedback} resetRate={resetRate} />
-      <Feedback
-        rate={rate}
-        totalFeedback={totalFeedback}
-        percentRate={percentRate}
-      />
-      <Notification rate={rate} />
-    </>
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options feedback={feedback} updateFeedback={updateFeedback} />
+      {totalFeedback > 0 ? <Feedback feedback={feedback} /> : <Notification />}
+    </div>
   );
 }
 
